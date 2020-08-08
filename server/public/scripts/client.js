7b1b93adc911082002781974e9edfd551c8bd12b
sukhanova <verusia.s@gmail.com>
2020-08-08 15:43:54 -0500

    added ajax call to add new task

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,10 +1,24 @@
 $(document).ready(readyNow);
 
 function readyNow() {
-  $("#submitBtn").on('click', addNewTask);
+  $("#submitBtn").on("click", addNewTask);
 }
 
 function addNewTask(event) {
     event.preventDefault();
-  console.log("#submitBtn clicked!");
+    console.log("#submitBtn clicked!");
+    const objectToSend = {
+        task: $('#newTaskIn').val(),
+    }
+    console.log('in addNewTask:', objectToSend);
+    $.ajax({
+        method: 'POST',
+        url: '/task',
+        data: objectToSend
+    }).then(function (response) { 
+        console.log(response);
+    }).catch(function (error) { 
+        alert(`error adding task`);
+        console.log(error);
+    })
 }
