491a8d9fa82263cae67de38800586f09e8d550e3
sukhanova <verusia.s@gmail.com>
2020-08-10 19:26:09 -0500

    heroku config

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -1,16 +1,34 @@
 // require
 const pg = require("pg");
-
+const url = require('url');
 //globals
 const Pool = pg.Pool;
-const config = {
-  database: "weekend-to-do-app",
-  host: "localhost",
-  port: 5432,
-  max: 12,
-  idleTimeOutMillis: 30000,
-}; //end config
+let config = {}; //end config
 const pool = new Pool(config);
+
+if (process.env.DATABASE_URL) {
+  const params = url.parse(process.env.DATABASE_URL);
+  const auth = params.auth.split(":");
+
+  config = {
+    user: auth[0],
+    password: auth[1],
+    host: params.hostname,
+    port: params.port,
+    database: params.pathname.split('/')[1],
+    ssl: true,
+    max: 10,
+    idleTimeoutMillis: 30000,
+  };
+} else {
+  config = {
+    host: "localhost",
+    port: 5432,
+    database: "weekend-to-do-app",
+    max: 10,
+    idleTimeoutMillis: 30000,
+  };
+}
 
 // db connection
 pool.on("connect", () => {
