eb87d11f391a6e20bd8f84055d7f467cc82a8641
sukhanova <verusia.s@gmail.com>
2020-08-10 20:25:51 -0500

    heroku config

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -1,43 +1,40 @@
-// require
+/* the only line you likely need to change is
+ database: 'prime_app',
+ change `prime_app` to the name of your database, and you should be all set!
+*/
 const pg = require("pg");
-const url = require('url');
-//globals
-const Pool = pg.Pool;
-let config = {}; //end config
-const pool = new Pool(config);
-
+const url = require("url");
+let config = {};
 if (process.env.DATABASE_URL) {
+  // Heroku gives a url, not a connection object
+  // https://github.com/brianc/node-pg-pool
   const params = url.parse(process.env.DATABASE_URL);
   const auth = params.auth.split(":");
-
   config = {
     user: auth[0],
     password: auth[1],
     host: params.hostname,
     port: params.port,
-    database: params.pathname.split('/')[1],
+    database: params.pathname.split("/")[1],
     ssl: { rejectUnauthorized: false },
-    max: 10,
-    idleTimeoutMillis: 30000,
+    max: 10, // max number of clients in the pool
+    idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
   };
 } else {
   config = {
-    host: "localhost",
-    port: 5432,
-    database: "weekend-to-do-app",
-    max: 10,
-    idleTimeoutMillis: 30000,
+    host: "localhost", // Server hosting the postgres database
+    port: 5432, // env var: PGPORT
+    database: "weekend-to-do-app", // CHANGE THIS LINE! env var: PGDATABASE, this is likely the one thing you need to change to get up and running
+    max: 10, // max number of clients in the pool
+    idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
   };
 }
-
-// db connection
-pool.on("connect", () => {
-  console.log("connected to db");
-}); // end db error
-
+// this creates the pool that will be shared by all other modules
+const pool = new pg.Pool(config);
+// the pool with emit an error on behalf of any idle clients
+// it contains if a backend error or network partition happens
 pool.on("error", (err) => {
-  console.log("ERROR connecting to DB:", err);
-}); //end db error
-
-// exports
+  console.log("Unexpected error on idle client", err);
+  process.exit(-1);
+});
 module.exports = pool;
